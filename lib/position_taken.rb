e137f540583dc5f5d2209ec661a14482a1e28042
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-31 23:17:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  return !!index || !(board[index] == " " || board[index] == "")
+  return index != nil || !(board[index] == " " || board[index] == "")
 end
