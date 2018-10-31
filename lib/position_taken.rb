9026f3c064c7a5922fa4ad335eaf4e50b8efd18e
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-31 23:23:16 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,6 @@
 def position_taken?(board, index)
   isNil = board[index] != nil
   hasEntry = !(board[index] == " " || board[index] == "")
-  return  isNil || hasEntry
+  #return  isNil || hasEntry
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
