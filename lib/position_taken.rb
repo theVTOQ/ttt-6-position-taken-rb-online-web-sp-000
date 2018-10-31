7e41be5dde2ab60e253665f5d9672d906498fa9d
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-31 23:23:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  #isNil = board[index] != nil
+  #hasEntry = !(board[index] == " " || board[index] == "")
+  #return  isNil || hasEntry
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
