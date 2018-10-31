000356e1ddd86ffd8bfe7eecbce3ae7e2ac751ad
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-31 23:20:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  isNil = !!index
+  isNil = index == nil
   hasEntry = !(board[index] == " " || board[index] == "")
   return  isNil && hasEntry
 end
